7b359a1727a0d6ab041dd9cdef4da6399bb6352a
Girish <girish@gmail.com>
2025-03-01 01:10:03 +0530

    changes

--- docker/app.js
+++ docker/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8989;
 
 app.get('/', (req, res) => {
   res.send('Hello from Dockerized Node.js app on Kubernetes!');
